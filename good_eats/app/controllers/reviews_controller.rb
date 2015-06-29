c60f36d72b59868bd58e70c449afc115298d8241
BrainFreeze <1briancunningham1@gmail.com>
2015-06-28 23:15:37 -0400

    week 6 system check

---
+++ good_eats/app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build(review_params)
+
+    if @review.save
+      flash[:notice] = "Success!"
+      redirect_to @restaurant
+    else
+      flash[:notice] = "There was an Error."
+      redirect_to @restaurant
+    end
+  end
+
+
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :body)
+    end
+end
---
+++ good_eats/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: 'Burger Town', address: '123 fake st', city: 'Boston', state: 'MA', zip_code: '12345')
+Restaurant.create(name: 'Sushi City', address: '123 fake st', city: 'Boston', state: 'MA', zip_code: '12345')
+Restaurant.create(name: 'Burrito Village', address: '123 fake st', city: 'Boston', state: 'MA', zip_code: '12345')
+Restaurant.create(name: 'Pizza Plaza', address: '123 fake st', city: 'Boston', state: 'MA', zip_code: '12345')
